3c0396ef48f9bbc5f2f34666cdac44cddf21dc06
Hodilton <amenohitottsunokami@yandex.ru>
2023-07-23 22:05:02 +0300

    Вынес говно в мусорку/отдельный класс с валидацией

--- Assets/SudokuGenerator.cs
+++ Assets/SudokuGenerator.cs
@@ -22,7 +22,6 @@ public class SudokuGenerator
 
     private int _index;
     private const int GRID_SIZE = 9;
-    private const int BOX_SIZE = 3;
 
     private const int DIFFICULTY_BASE = 30;
 
@@ -34,11 +33,7 @@ public class SudokuGenerator
 
         CreateGrid(cells);     
         GenerateGrid(cells);
-        CleanRandomCell(cells, DIFFICULTY_BASE);
-        //BlockAllFillCell(cells);
-        
-        //DrawBaseGrid(cells);
-        //MixGrid(cells);       
+        CleanRandomCell(cells, DIFFICULTY_BASE);    
 
         return cells;
     }
@@ -69,20 +64,6 @@ public class SudokuGenerator
         }
     }
 
-    private void BlockAllFillCell(Cell[,] cells)
-    {
-        for (var row = 0; row < GRID_SIZE; row++)
-        {
-            for (var col = 0; col < GRID_SIZE; col++)
-            {
-                if (cells[row, col].Value != 0)
-                {
-                    cells[row, col].IsActive = false;
-                }
-            }
-        }
-    }
-
     #region GenerateGrid
 
     private bool GenerateGrid(Cell[,] cells)
@@ -99,7 +80,7 @@ public class SudokuGenerator
         {
             emptyCell.Value = mixedArray[i];
 
-            if (!ValidateCell(cells, emptyCell))
+            if (!SudokuValidator.ValidateCell(cells, emptyCell))
             {
                 continue;
             }
@@ -150,154 +131,4 @@ public class SudokuGenerator
     }
 
     #endregion
-
-    #region Validate
-
-    public static bool ValidateCell(Cell[,] cells, Cell cell)
-    {
-        return ValidateCol(cells, cell)
-            && ValidateRow(cells, cell)
-            && ValidateBox(cells, cell);
-    }
-
-    private static bool ValidateCol(Cell[,] cells, Cell cell)
-    {
-        for(int i = 0; i < GRID_SIZE; i++)
-        {
-            if (cells[i, cell.y].Value == cell.Value && i != cell.x)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static bool ValidateRow(Cell[,] cells, Cell cell)
-    {
-        for (int i = 0; i < GRID_SIZE; i++)
-        {
-            if (cells[cell.x, i].Value == cell.Value && i != cell.y)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static bool ValidateBox(Cell[,] cells, Cell cell)
-    {
-        int xBox = (cell.x / BOX_SIZE) * BOX_SIZE;
-        int yBox = (cell.y / BOX_SIZE) * BOX_SIZE;
-
-        for (int i = xBox; i < xBox + BOX_SIZE; i++)
-        {
-            for (int j = yBox; j < yBox + BOX_SIZE; j++)
-            {
-                if (cells[i, j].Value == cell.Value && (i != cell.x && j != cell.y))
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    #endregion
-
-    #region MixGrid
-
-    private void DrawBaseGrid(Cell[,] cells)
-    {
-        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        const int shift = 3, shiftTransition = 4;
-
-        Index = 0;
-        for (int i = 0; i < GRID_SIZE; i++)
-        {
-            for (int j = 0; j < GRID_SIZE; j++)
-            {
-                Index++;
-                cells[i, j].Value = nums[Index - 1];
-            }
-
-            if ((i + 1) % BOX_SIZE == 0)
-            {
-                Index += shiftTransition;
-            }
-            else
-            {
-                Index += shift;
-            }
-        }
-    }
-
-    private void MixGrid(Cell[,] cells)
-    {
-        Action<Cell[,]>[] mixFunctions = {
-            TranspositionGrid,
-            SwapRandomRows,
-            SwapRandomCols
-        };
-
-        int numberMixes = random.Next(25, 45);
-        for (int i = 0; i < numberMixes; i++)
-        {
-            int functionIndex = random.Next(0, mixFunctions.Length);
-            mixFunctions[functionIndex](cells);
-        }
-    }
-
-    private void SwapRandomCols(Cell[,] cells)
-    {
-        int area = random.Next(0, 3);
-        int row1 = random.Next(1, 4), row2;
-        do { row2 = random.Next(1, 4);
-        } while (row2 == row1);
-        SwapSelectedRows(row1 - 1 + area * BOX_SIZE, row2 - 1 + area * BOX_SIZE, cells);
-    }
-
-    private void SwapRandomRows(Cell[,] cells)
-    {
-        int area = random.Next(0, 3);
-        int row1 = random.Next(1, 4), row2;
-        do { row2 = random.Next(1, 4);
-        } while (row2 == row1);
-        SwapSelectedCols(row1 - 1 + area * BOX_SIZE, row2 - 1 + area * BOX_SIZE, cells);
-    }
-
-    private void TranspositionGrid(Cell[,] cells)
-    {
-        for (int i = 0; i < GRID_SIZE; i++)
-        {
-            for (int j = 0 + i; j < GRID_SIZE; j++)
-            {
-                Swap(ref cells[i, j], ref cells[j, i]);
-            }
-        }
-    }
-
-    private void SwapSelectedCols(int n, int m, Cell[,] cells) 
-    {
-        for(int i = 0; i < GRID_SIZE; i++)
-        {
-            Swap(ref cells[n, i], ref cells[m, i]);
-        }
-    }
-
-    private void SwapSelectedRows(int n, int m, Cell[,] cells)
-    {
-        for (int i = 0; i < GRID_SIZE; i++)
-        {
-           Swap(ref cells[i, n], ref cells[i, m]);
-        }
-    }
-
-    private void Swap(ref Cell a, ref Cell b)
-    {
-        var temp = a.Value;
-        a.Value = b.Value;
-        b.Value = temp;
-    }
-
-    #endregion MIXGRID
 }
---
+++ Assets/SudokuValidator.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class SudokuValidator : MonoBehaviour
+{
+    private const int BOX_SIZE = 3;
+    private const int GRID_SIZE = 9;
+
+    public static bool ValidateCell(Cell[,] cells, Cell cell)
+    {
+        return ValidateCol(cells, cell)
+            && ValidateRow(cells, cell)
+            && ValidateBox(cells, cell);
+    }
+
+    private static bool ValidateCol(Cell[,] cells, Cell cell)
+    {
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            if (cells[i, cell.y].Value == cell.Value && i != cell.x)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool ValidateRow(Cell[,] cells, Cell cell)
+    {
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            if (cells[cell.x, i].Value == cell.Value && i != cell.y)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool ValidateBox(Cell[,] cells, Cell cell)
+    {
+        int xBox = (cell.x / BOX_SIZE) * BOX_SIZE;
+        int yBox = (cell.y / BOX_SIZE) * BOX_SIZE;
+
+        for (int i = xBox; i < xBox + BOX_SIZE; i++)
+        {
+            for (int j = yBox; j < yBox + BOX_SIZE; j++)
+            {
+                if (cells[i, j].Value == cell.Value && (i != cell.x && j != cell.y))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
